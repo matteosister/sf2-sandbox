30885d952d143c01489f739152e33f98d8f6eae8
matteo giachino <matteo.giachino@facile.it>
2013-01-25 14:58:52 +0100

    git-all automatic commit

--- src/Facile/SandboxBundle/Controller/DefaultController.php
+++ src/Facile/SandboxBundle/Controller/DefaultController.php
@@ -9,12 +9,21 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends BaseController
 {
     /**
-     * @Route("/home.{_format}", name="homepage",
+     * @Route("/", name="homepage")
+     * @Template
+     */
+    public function homepageAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/list.{_format}", name="lista_pagine",
      *   defaults={"_format"="html"}
      * )
      * @Template()
      */
-    public function indexAction()
+    public function listAction()
     {
         $pages = $this->getPageRepository()->findAll();
 
